80bdab3fc522361a5c8d76e91fc4230cecaae713
A G Sanjay Ram <a.g.sanjayram@gmail.com>
2022-06-04 11:14:44 +0530

    mian

---
+++ day 20/incremen.js
@@ -0,0 +1,8 @@
+//increment, decrement
+
+var first =1;
+var sec = first++;
+var third = ++first;
+console.log(sec,third);
+
+console.log(2**8);//power opertor
---
+++ day 20/string.js
@@ -0,0 +1,31 @@
+var name= "Array is lis";
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.slice(0,2));
+console.log(name.replace("lis","lie"));
+console.log(name.includes("is"));
+
+var number ="123546";
+
+console.log(parseInt(number));
+
+//operator
+var a=1; //assignment operator
+var b=2;
+a+=b;
+console.log(a);
+a= a+b;
+console.log(a);
+b-=a;
+console.log(b);
+a*=b;
+console.log(a);
+a/=b;
+console.log(a);
+//modulus opereato
+
+//in single line we add many value by uisng comma 
+console.log(a,b);
+
+//arthematic
+//+,-,/,*
