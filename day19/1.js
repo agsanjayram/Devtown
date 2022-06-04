ca9c07c256a70e600eae2299dfd007291de36c1b
A G Sanjay Ram <a.g.sanjayram@gmail.com>
2022-06-04 10:45:08 +0530

    main

---
+++ day19/1.js
@@ -0,0 +1,40 @@
+//basis of js
+console.log("hello");
+console.log(123);
+console.log(true);//bollean values
+console.log(undefined);//undefined
+console.log(null);
+
+//mixed array
+console.log("array",["12","32",21,true ]);
+// array
+console.log("array",["12","32","212" ]); //not stored accutally
+
+//create variable
+
+let name = ["arun","main","hrkw"];
+
+console.log(name);
+console.log(name[2]);
+
+//object
+var studname={
+    name:"hello",
+    age:"23",
+     "lets hello" :"start"
+}
+
+console.log(studname["lets hello"]);
+console.log(studname.age);
+// no define it became undefined not null
+var number=null;
+console.log(number);
+number = "devtown";
+console.log(number);
+number=[1234];
+console.log(number);
+
+//compare
+console.log(2==3);
+console.log(false=="");
+c
